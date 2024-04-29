811178c24b6eb9f97661a22f100da8e1f4deee2f
Sironi-00 <00sironi@gmail.com>
2024-04-29 12:14:41 +0100

    cors config:0.3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors({origin: `*`}));
+// app.use(cors({origin: `*`}));
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
