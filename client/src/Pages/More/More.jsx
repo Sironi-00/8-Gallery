4328f6d6bb1f0f8103d9282ae9ab2b16dfa90035
Sironi-00 <00sironi@gmail.com>
2024-05-25 00:12:13 +0100

    Add Pages/More for detailing the app

---
+++ client/src/Pages/More/More.jsx
@@ -0,0 +1,26 @@
+export default function More() {
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col">
+                    <h3>More page</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <p className="">
+                        Infinity gallery is a project designed to share images. It was built with my sibling and friend in mind, I wanted to give them a small space where they could display their art without much competition.
+                    </p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <p className="m-0 p-0">Info on this project and guidelines</p>
+                </div>
+                <div className="col border">
+                    <p className="m-0 p-0">More text and such</p>
+                </div>
+            </div>
+        </div>
+    );
+}
