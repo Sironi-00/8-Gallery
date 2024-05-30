02d60423093967d2a948551bcecf8e651909e5c2
Sironi-00 <00sironi@gmail.com>
2024-05-30 15:51:30 +0100

    add clear btn for search

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,21 +1,34 @@
 import { useContext, useEffect, useState } from "react";
 import { SearchContext } from "../../ContextProvider/ContextProvider";
+import BackspaceIcon from "@mui/icons-material/Backspace";
 
 export default function SearchBar() {
     const { updateSearchString } = useContext(SearchContext);
     const [searchText, setSearchText] = useState("");
+
+    const handleSearchClear = (e) => {
+        e.preventDefault();
+        setSearchText("");
+    };
 
     useEffect(() => {
         updateSearchString(searchText);
     }, [searchText, updateSearchString]);
 
     return (
-        <input
-            type="search"
-            className="form-control me-2 rounded"
-            placeholder="Search"
-            value={searchText}
-            onChange={({ target }) => setSearchText(target.value)}
-        />
+        <div className="input-group">
+            <input
+                type="search"
+                className="form-control"
+                placeholder="Search"
+                value={searchText}
+                onChange={({ target }) => setSearchText(target.value)}
+            />
+            <span className="input-group-text p-0">
+                <button type="" className="btn m-0 " onClick={handleSearchClear}>
+                    <BackspaceIcon />
+                </button>
+            </span>
+        </div>
     );
 }
